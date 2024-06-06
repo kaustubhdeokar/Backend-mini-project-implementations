25bc46ed886d125218118708bc54cb20f9fc7cc9
kaustubhdeokar <kaustubhd9@gmail.com>
2024-06-06 18:56:14 +0530

    kafka-implementation: client-app receiving updates from a messaging queue

--- kafka/user/src/main/java/com/bookacab/user/controller/UserController.java
+++ kafka/user/src/main/java/com/bookacab/user/controller/UserController.java
@@ -1,2 +1,30 @@
-package com.bookacab.user.controller;public class UserController {
+package com.bookacab.user.controller;
+
+import com.bookacab.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user/update")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/receive")
+    public ResponseEntity<List<String>> getLocationUpdate(){
+        List<String> location = userService.fetchDriverLocation();
+        return new ResponseEntity<>(location, HttpStatus.OK);
+    }
+
 }
--- kafka/user/src/main/java/com/bookacab/user/service/UserService.java
+++ kafka/user/src/main/java/com/bookacab/user/service/UserService.java
@@ -1,4 +1,31 @@
 package com.bookacab.user.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class UserService {
+
+    private List<String> locations = new ArrayList<>();
+
+    public void UserService() {
+    }
+
+    public List<String> fetchDriverLocation() {
+        int size = locations.size();
+        if (size > 5) {
+            locations = locations.subList(locations.size() - 5, locations.size());
+        }
+        return locations;
+    }
+
+    @KafkaListener(topics = "location", groupId = "user-group-1")
+    public void fetchDriverLocation(String location) {
+        locations.add(location);
+    }
+
 }

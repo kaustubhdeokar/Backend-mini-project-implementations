eee5abcfd6b3edfa4b17b6f5f0f05d9955651d90
kaustubhdeokar <kaustubhd9@gmail.com>
2024-06-18 22:00:40 +0530

    redis - accepting multiple connections

--- redis/app/server.go
+++ redis/app/server.go
@@ -2,34 +2,42 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	connection, err := listener.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer listener.Close()
+	fmt.Println("Listening on port")
+
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+		fmt.Println("New connection")
+		go handleConnection(connection)
 	}
-	buf := make([]byte, 11024)
-	_, err1 := connection.Read(buf)
-	if err1 != nil {
-		fmt.Println("Error reading from connection: ", err1.Error())
-		os.Exit(1)
+}
+
+func handleConnection(connection net.Conn) {
+	defer connection.Close()
+	for {
+		buffer := make([]byte, 1024)
+		n, err := connection.Read(buffer)
+		if err != nil {
+			fmt.Println("Error reading from connection: ", err.Error())
+			return
+		}
+		fmt.Println("Received: ", string(buffer[0:n]))
+		message := []byte("+PONG\r\n")
+		connection.Write(message)
 	}
-	fmt.Println("read something")
-	message := []byte("+PONG\r\n")
-	connection.Write(message)
 }
